0ada6adb1fa5e523aa5fe09dd506410d47e4c3c5
Brendan Hagan <bhaganksu@gmail.com>
2019-01-05 22:46:09 -0500

    feat: add utm_source to scryfall links

--- assets/js/scryfall.js
+++ assets/js/scryfall.js
@@ -2,7 +2,8 @@
 // https://img.scryfall.com/cards/large/en/mmq/61.jpg?1517813031
 
 (() => {
-    const scryfallLinks = document.querySelectorAll(`a[href*="scryfall.com/card/"]`);
+    const scryfallLinks = document.querySelectorAll(`a[href*="scryfall.com"]`);
+    const scryfallCardLinks = document.querySelectorAll(`a[href*="scryfall.com/card/"]`);
 
     document.querySelector('body').insertAdjacentHTML('beforeend', `<img id="scryfall-tooltip" style="display: block; position: absolute; width: 250px; height: auto; border-radius: 11px;"/>`);
     const scryfallTooltip = document.querySelector('#scryfall-tooltip');
@@ -23,11 +24,21 @@
         document.removeEventListener('mousemove', mouseMoveEvent);
         scryfallTooltip.src = '';
         scryfallTooltip.style.display = 'none';
-    }
+    };
 
+    // Add 'hextended' as the utm_source.
     scryfallLinks.forEach(link => {
+        const url = new URL(link.href);
+        const urlSearchParams = new URLSearchParams(url.search);
+        urlSearchParams.set('utm_source', 'hextended');
+        url.search = urlSearchParams.toString();
+        link.href = url;
+    });
+
+    // Set the autocard target and listeners.
+    scryfallCardLinks.forEach(link => {
         const path = new URL(link.href).pathname.split('/');
-        const imageUrl = `https://img.scryfall.com/cards/normal/en/${path[2]}/${path[3]}.jpg`;
+        const imageUrl = `https://img.scryfall.com/cards/normal/en/${path[2]}/${path[3]}.jpg?utm_source=hextended`;
         link.classList.add('scryfall-hover');
 
         link.addEventListener('mouseover', () => {
